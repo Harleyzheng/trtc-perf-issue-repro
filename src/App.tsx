ca863b3620ae2e53038237fb2ab89ee1c3c4c41b
Harleyzheng <zhengharley@gmail.com>
2022-09-13 17:12:10 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import TRTC from "trtc-js-sdk";
 
-function App() {
+export default function App() {
+  console.info("123", TRTC);
+  // mock DOM
+  const a = new Array(2000).fill(Math.random());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box
+        sx={{
+          ".box": {
+            width: 200,
+            margin: "0 auto",
+            border: "1px solid #ff6700",
+            overflow: "hidden",
+            position: "absolute",
+          },
+
+          ".animate": {
+            paddingLeft: "20px",
+            color: "#000",
+            display: "inline-block",
+            whiteSpace: "nowrap",
+            animation: "10s wordsLoop linear infinite normal",
+          },
+
+          "@keyframes wordsLoop": {
+            "0%": {
+              transform: "translateX(200px) rotateZ(360deg)",
+            },
+            "100%": {
+              transform: "translateX(-100%) rotateZ(360deg)",
+            },
+          },
+        }}
+      >
+        <div className="box">
+          <p className="animate">13123123123</p>
+        </div>
+      </Box>
+      {a.map((v, i) => (
+        <div key={i}>{v}</div>
+      ))}
+    </>
   );
 }
-
-export default App;
